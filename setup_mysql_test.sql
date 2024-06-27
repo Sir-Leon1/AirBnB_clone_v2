ad55c97c2d1bd7881a8eda5299aa7f61fc2d6621
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-06-27 14:58:35 +0300

    Setting Up sql db for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,18 @@
+-- Creates a MYSQL Database hbnb_test_db
+-- A new user hbnb_test in localhost
+-- hbnb_trest should have all privileges on the db hbnb_test_db
+-- hbnb_test should have SELECT privilege on the db performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+    ON `hbnb_test_db`.*
+    TO 'hbnb_test'@'localhost'
+        IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+    ON `performance_schema`.*
+    TO 'hbnb_test'@'localhost'
+        IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
