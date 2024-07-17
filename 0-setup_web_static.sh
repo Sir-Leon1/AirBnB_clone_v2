7a426de1712dac914ebbe54ce1d5a42d5d949afc
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-17 16:55:45 +0300

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,14 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -15,7 +22,6 @@ chgrp -R ubuntu /data/
 printf %s "server {
 	listen	80 default_server;
 	listen	[::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
 	root	/var/www/html;
 	index	index.html index.htm;
 
@@ -33,6 +39,10 @@ printf %s "server {
 	    root /var/www/html;
 	    internal;
 	}
+
+	location / {
+	    add_header X-Served-By $HOSTNAME;
+}
 }
 " > /etc/nginx/sites-available/default
 
