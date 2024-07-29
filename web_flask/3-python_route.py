c2b3cbc6b53a834e253c8e8f595bc8f1e4575e0c
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-29 17:31:50 +0300

    Corrected default twxt for /python/ endpoint

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -36,6 +36,7 @@ def c(text):
     return f"C {text.replace('_', ' ')}"
 
 
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     """Route that returns 'Python <text>' with underscores replaced by spaces.
